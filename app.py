822d7d97c42d06d2ea3a99720d95ff3ea4913bc5
p_duanmei <p_duanmei@tencent.com>
2021-06-15 15:23:31 +0800

    新增功能

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 from tools.ThirdModuleInstall import ThirdModule
 
 app = Flask(__name__)
@@ -10,14 +10,18 @@ def hello():
     return "Hello world"
 
 
-@app.route("/install_mysql/singleton", methods=['Get', 'POST'])
+@app.route("/install_mysql/singleton", methods=['POST'])
 def install_mysql_singleton():
-    user = request.form['username']
-    pwd = request.form['password']
-    ip = request.form['ip']
-    port = request.form['port'] or '3306'
-    tm = ThirdModule(ip, port, user, pwd)
-    tm.scp_mysql_package()
+    user = request.form.get("username")
+    pwd = request.form.get('password')
+    ip = request.form.get('ip')
+    port = int(request.form.get('port') or '22')
+    data_dir = request.form.get('datadir')
+    print("%s,%s,%s,%s,%s" % (user, pwd, ip, port, data_dir))
+    tm = ThirdModule(ip, port, user, pwd, data_dir)
+    res = tm.install_mysql_node()
+    print('res:', res)
+    return "Hello world"
 
 
 if __name__ == '__main__':
--- tools/ThirdModuleInstall.py
+++ tools/ThirdModuleInstall.py
@@ -4,11 +4,12 @@ import paramiko
 
 
 class ThirdModule(object):
-    def __init__(self, ip=None, port=22, user=None, password=None):
+    def __init__(self, ip=None, port=22, user=None, password=None, data_dir=None):
         self.ip = ip
         self.port = port
         self.user = user
         self.password = password
+        self.data_dir = data_dir
 
     def _connect(self):
         ssh = paramiko.SSHClient()
@@ -24,13 +25,13 @@ class ThirdModule(object):
         return m_str
 
     # scp mysql-8.0.19-linux-glibc2.12-x86_64.tar.xz to remote server
-    def scp_mysql_package(self):
+    def _scp_mysql_package(self):
         ssh = self._connect()
         transport = ssh.get_transport()
         sftp = paramiko.SFTPClient.from_transport(transport)
         mysql_package \
             = os.path.abspath(os.path.dirname(os.getcwd())) \
-            + '/package/mysql-8.0.25-linux-glibc2.17-x86_64-minimal.tar.xz'
+            + '/AOA/package/mysql-8.0.25-linux-glibc2.17-x86_64-minimal.tar.xz'
         sftp.put(mysql_package, '/usr/local/src/mysql-8.0.25-linux-glibc2.17-x86_64-minimal.tar.xz')
         ssh.close()
         sftp.close()
@@ -40,7 +41,7 @@ class ThirdModule(object):
     # mv mysql-8.0.19-linux-glibc2.12-x86_64 /usr/local/mysql8.0
     # echo 'export  PATH=/usr/local/mysql8.0/bin:$PATH' >> /etc/profile
     # . /etc/profile
-    def install_single_mysql(self):
+    def _install_single_mysql(self):
         command = "cd /usr/local/src && tar xf mysql-8.0.25-linux-glibc2.17-x86_64-minimal.tar.xz;" \
                   "cd /usr/local/src && mv mysql-8.0.25-linux-glibc2.17-x86_64-minimal /usr/local/mysql8.0;" \
                   "echo 'export  PATH=/usr/local/mysql8.0/bin:$PATH' >> /etc/profile;" \
@@ -48,8 +49,8 @@ class ThirdModule(object):
         # command = ". /etc/profile"
         ssh = self._connect()
         stdin, stdout, stderr = ssh.exec_command(command)
-        print(stderr.read())
         ssh.close()
+        return stderr.read()
 
     # useradd -M -s /sbin/nologin mysql
     # mkdir -pv /datadir/{temp, log, data}
@@ -57,17 +58,18 @@ class ThirdModule(object):
     # chown -R mysql:mysql /datadir
     # mysqld --initialize-insecure --datadir=/datadir/data --user = mysql
     # chown -R mysql: mysql /datadir
-    def init_single_mysql(self, data_dir):
+    def _init_single_mysql(self):
         command = "useradd -M -s /sbin/nologin mysql;" \
                   "mkdir -pv /%s/{temp,log,data};" \
                   "touch /%s/log/err.log;" \
                   "chown -R mysql:mysql /%s;" \
                   "/usr/local/mysql8.0/bin/mysqld --initialize-insecure --datadir=/%s/data --user=mysql;" \
-                  "chown -R mysql:mysql /%s" % (data_dir, data_dir, data_dir, data_dir, data_dir)
+                  "chown -R mysql:mysql /%s" % \
+                  (self.data_dir, self.data_dir, self.data_dir, self.data_dir, self.data_dir)
         ssh = self._connect()
         stdin, stdout, stderr = ssh.exec_command(command)
-        print(stderr.read())
         ssh.close()
+        return stderr.read()
 
     # [mysqld]
     # basedir=/usr/local/mysql8.0
@@ -90,29 +92,29 @@ class ThirdModule(object):
     #
     # [client]
     # socket=/datadir/temp/mysqld.sock
-    def config_start_single_mysql(self, data_dir):
+    def _config_start_single_mysql(self):
         config = configparser.ConfigParser()
         config['mysqld'] = {
             'basedir': '/usr/local/mysql8.0',
             'user': 'mysql',
             'port': '3306',
             'mysqlx_port': '33060',
-            'datadir': '/%s/data' % (data_dir, ),
-            'log-error': '/%s/log/err.log' % (data_dir, ),
-            'pid-file': '/%s/temp/mysqld.pid' % (data_dir, ),
-            'socket': '/%s/temp/mysqld.sock' % (data_dir, ),
-            'mysqlx_socket': '/%s/temp/mysqlx.sock' % (data_dir, ),
+            'datadir': '/%s/data' % (self.data_dir, ),
+            'log-error': '/%s/log/err.log' % (self.data_dir, ),
+            'pid-file': '/%s/temp/mysqld.pid' % (self.data_dir, ),
+            'socket': '/%s/temp/mysqld.sock' % (self.data_dir, ),
+            'mysqlx_socket': '/%s/temp/mysqlx.sock' % (self.data_dir, ),
             'symbolic-links': '0',
             'server_id': '%s' % self._ip_2_server_id(),
             'gtid-mode': 'on',
             'enforce-gtid-consistency': 'true',
             'skip_ssl': '1',
-            'log_bin': '/%s/log/binlog' % (data_dir, ),
+            'log_bin': '/%s/log/binlog' % (self.data_dir, ),
             'binlog_format': 'ROW',
             'secure_file_priv': '',
         }
         config['client'] ={
-            'socket': '/%s/temp/mysqld.sock' % (data_dir, )
+            'socket': '/%s/temp/mysqld.sock' % (self.data_dir, )
         }
         with open('my.cnf', 'w') as f:
             config.write(f)
@@ -124,22 +126,59 @@ class ThirdModule(object):
         stdin, stdout, stderr = ssh.exec_command(command)
         sftp.close()
         ssh.close()
-        print(stderr.read())
+        return stderr.read()
 
-    def set_master_node(self):
+    def _set_master_node(self):
         ssh = self._connect()
-        command = "mysql -uroot -e CREATE USER 'repl'@'%' IDENTIFIED BY 'password';" \
-                  "mysql -uroot -e GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';"
-        ssh.exec_command(command)
+        command = "/usr/local/mysql8.0/bin/mysql -uroot -e " \
+                  "\"CREATE USER 'repl'@'%' IDENTIFIED BY 'password';GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%'\";"
+        stdin, stdout, stderr = ssh.exec_command(command)
         ssh.close()
+        return stderr.read()
 
-    def set_slave_node(self):
-        pass
+    def _set_slave_node(self, m_ip):
+        ssh = self._connect()
+        command = "CHANGE MASTER TO MASTER_HOST = '%s'," \
+                  "MASTER_PORT = 3306, " \
+                  "MASTER_USER = 'repl', " \
+                  "MASTER_PASSWORD = 'password', " \
+                  "MASTER_AUTO_POSITION = 1;start slave;" % (m_ip, )
+        stdin, stdout, stderr = ssh.exec_command(command)
+        ssh.close()
+        return stderr.read()
+
+    def install_mysql_node(self):
+        self._scp_mysql_package()
+        res = self._install_single_mysql()
+        if res != '':
+            return res
+        res = self._init_single_mysql()
+        if res != '':
+            return res
+        res = self._config_start_single_mysql()
+        if res != '':
+            return res
+
+    def install_master_mysql_node(self):
+        res = self.install_mysql_node()
+        if res != "":
+            res = self._set_master_node()
+        return res
+
+    def install_slave_mysql_node(self, m_ip):
+        res = self.install_mysql_node()
+        if res != "":
+            res = self._set_slave_node(m_ip)
+        return res
 
 
 if __name__ == '__main__':
-    tm = ThirdModule('192.168.3.128', 22, 'root', 'daleD1991')
-    tm.scp_mysql_package()
-    tm.install_single_mysql()
-    tm.init_single_mysql('datadir')
-    tm.config_start_single_mysql('datadir')
+    m = ThirdModule('192.168.3.128', 22, 'root', 'daleD1991', 'datadir')
+    s = ThirdModule('192.168.3.129', 22, 'root', 'daleD1991', 'datadir')
+    # tm.scp_mysql_package()
+    # tm.install_single_mysql()
+    # tm.init_single_mysql('datadir')
+    # tm.config_start_single_mysql('datadir')
+    # tm.set_master_node()
+    m.install_master_mysql_node()
+    s.install_slave_mysql_node(m.ip)
